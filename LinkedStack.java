f2a9c55f82545671686693e1af302e25a9ae9c56
Christopher <programmerchristopher@gmail.com>
2021-10-05 19:25:08 -0700

    almost finished stacks; ArrayStack has 1 stub

--- LinkedStack.java
+++ LinkedStack.java
@@ -12,9 +12,36 @@ public final class LinkedStack<T> implements StackInterface<T>
    {
       topNode = null;
    }
-  
-//  < Implementations of the stack operations go here. >
-//  . . .
+   public void push(T newEntry)
+   {
+      topNode = new Node(newEntry, topNode);
+   }
+
+   public T pop()
+   {
+      T top = peek();  // Might throw EmptyStackException
+      // Assertion: topNode != null
+      topNode = topNode.getNextNode();
+      return top;
+   }
+
+   public T peek()
+   {
+      if (isEmpty())
+         throw new EmptyStackException();
+      else
+         return topNode.getData();
+   }
+
+   public boolean isEmpty()
+   {
+      return topNode == null;
+   }
+   
+   public void clear()
+   {
+      topNode = null;
+   }
 
 	private class Node
 	{
--- ResizeableArrayStack.java
+++ ResizeableArrayStack.java
@@ -1,9 +1,11 @@
+import java.util.EmptyStackException;
+import java.util.Arrays;
 /**
     A class of stacks whose entries are stored in an array.
     @author Frank M. Carrano and Timothy M. Henry
     @version 5.0
 */
-public final class ArrayStack<T> implements StackInterface<T>
+public final class ResizeableArrayStack<T> implements StackInterface<T>
 {
 	private T[] stack;    // Array of stack entries
 	private int topIndex; // Index of top entry
@@ -11,12 +13,12 @@ public final class ArrayStack<T> implements StackInterface<T>
 	private static final int DEFAULT_CAPACITY = 50;
 	private static final int MAX_CAPACITY = 10000;
   
-   public ArrayStack()
+   public ResizeableArrayStack()
    {
       this(DEFAULT_CAPACITY);
    }
   
-   public ArrayStack(int initialCapacity)
+   public ResizeableArrayStack(int initialCapacity)
    {
       integrityOK = false;
       checkCapacity(initialCapacity);
@@ -28,9 +30,75 @@ public final class ArrayStack<T> implements StackInterface<T>
 		topIndex = -1;
       integrityOK = true;
   }
-  
-//  < Implementations of the stack operations go here. >
-//  < Implementations of the private methods go here; checkCapacity and checkIntegrity
-//    are analogous to those in Chapter 2. >
-//  . . .
+
+   public void push(T newEntry)
+   {
+      checkIntegrity();
+      ensureCapacity();
+      stack[topIndex + 1] = newEntry;
+      topIndex++;
+   }
+
+   private void ensureCapacity()
+   {
+      if (topIndex >= stack.length - 1) // If array is full, double its size
+      {
+         int newLength = 2 * stack.length;
+         checkCapacity(newLength);
+         stack = Arrays.copyOf(stack, newLength);
+      }
+   }
+
+   public T pop()
+   {
+      checkIntegrity();
+      if (isEmpty())
+         throw new EmptyStackException();
+      else
+      {
+         T top = stack[topIndex];
+         stack[topIndex] = null;
+         topIndex--;
+         return top;
+      }
+   }
+
+   public T peek()
+   {
+      checkIntegrity();
+      if (isEmpty())
+         throw new EmptyStackException();
+      else
+         return stack[topIndex];
+   }
+
+   public boolean isEmpty()
+   {
+      return topIndex < 0;
+   }
+   
+   public void clear()
+   {
+      checkIntegrity();
+      
+      // Remove references to the objects in the stack,
+      // but do not deallocate the array
+      while (topIndex > -1)
+      {
+         stack[topIndex] = null;topIndex--;
+      }
+      //Assertion: topIndex is -1
+   }
+
+   private void checkCapacity(int newLength)
+   {
+      // STUB
+   }
+
+   // Throws an exception if this object is not initialized.
+   private void checkIntegrity()
+   {
+      if (!integrityOK)
+         throw new SecurityException("ArrayBag object is corrupt.");
+   }
 }
